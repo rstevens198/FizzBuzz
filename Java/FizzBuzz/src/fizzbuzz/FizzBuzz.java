98249d5816aeab03a2aceb020078185d29d35958
Ryan Stevens <ryanstevens198@yahoo.com>
2016-05-21 06:32:07 -0700

    updated lessons learned

--- Java/FizzBuzz/src/fizzbuzz/FizzBuzz.java
+++ Java/FizzBuzz/src/fizzbuzz/FizzBuzz.java
@@ -8,7 +8,11 @@ package fizzbuzz;
  * and if it i divisible by 3 and 5 it will output Fizz Buzz. Even though this can be done in one main function it must
  * include at least two functions. Even though we can use a for loop and i must declare a variable.
  * 
- * This is a timed assignment with 10 minutes to complete. Because of this we do not care about efficiency. 
+ * This is a timed assignment with 10 minutes to complete. Because of this we do not care about efficiency.
+ * 
+ * Lessons Learned: After not doing java for a year it was a little struggle getting used to the syntax.
+ * I had to google different sntax and also I have never used netbeans for java and am used to eclipse so
+ * I ran into a issue with netbeans finding the main function. 
  * 
  */
 public class FizzBuzz {
